be6555915cbc104a43872ee22d05b3079b0f88cc
Roman <romanwn@mail.com>
2024-01-15 23:04:13 +0200

    Added core functionality, map each file path from source to a flag if it exists or not in replica

--- sync_source_replica.py
+++ sync_source_replica.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 from os import walk
+from os.path import exists, join, relpath
 
 source_folder_path = "C:/Users/yop/Desktop/w/assingments/Veeam/source"
 replica_folder_path = "C:/Users/yop/Desktop/w/assingments/Veeam/replica"
@@ -29,3 +30,31 @@ def remove_file(file_path):
 
 def remove_folder(folder_path):
     os.rmdir(folder_path)
+
+
+def operational_tree(source_folder_path, replica_folder_path, topdown=True):
+    """
+    Creates two dictionaries which contains a filepath as key and a boolean as value. The
+    flag represents wheather or not the file is present in the replica folder.
+    When copying files to replica topdown=True, when deleting from replica topdown=False 
+    """
+    files_dictionary = {}
+    folder_dictionary = {}
+    for dirpath, dirnames, filenames in walk(source_folder_path, topdown=topdown):
+       
+        relative_path = relpath(dirpath, source_folder_path)
+        for dirname in dirnames:
+            relative_path_to_folder = join(relative_path, dirname)
+            if exists(join(replica_folder_path, relative_path_to_folder)):
+                folder_dictionary[relative_path_to_folder] = True
+            else:
+                folder_dictionary[relative_path_to_folder] = False
+
+        for filename in filenames:
+            relative_path_to_file = join(relative_path, filename)
+            if exists(join(replica_folder_path, relative_path_to_file)):
+                files_dictionary[relative_path_to_file] = True
+            else:
+                files_dictionary[relative_path_to_file] = False
+    
+    return folder_dictionary, files_dictionary
