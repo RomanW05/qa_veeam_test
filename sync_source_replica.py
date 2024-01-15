01358d20bd1897451de32b796b6467cf4975369d
Roman <romanwn@mail.com>
2024-01-15 23:07:22 +0200

    Added functions to perform the actions. Do what it is supposed to with the help of dictionaries, sync files and folders

--- sync_source_replica.py
+++ sync_source_replica.py
@@ -58,3 +58,41 @@ def operational_tree(source_folder_path, replica_folder_path, topdown=True):
                 files_dictionary[relative_path_to_file] = False
     
     return folder_dictionary, files_dictionary
+
+def copy_all_files_from_source(source_folder_path, replica_folder_path, folders_to_create, files_to_create):
+    for elem in folders_to_create.items():
+        replica_folder = join(replica_folder_path, elem[0])
+        if not exists(replica_folder):
+            create_folder(replica_folder)
+            print(f'Created: {replica_folder}')
+
+    for elem in files_to_create.items():
+        source_file = join(source_folder_path, elem[0])
+        replica_file = join(replica_folder_path, elem[0])
+        if elem[1]:  # File does exist
+            source_checksum = checksum_file(source_file)
+            replica_cheksum = checksum_file(replica_file)
+            if source_checksum != replica_cheksum:  # Files are different
+                # Copying file log
+                create_file(source_file, replica_file)
+                print(f'Modified: {source_file}')
+            else:
+                pass  # Ignore exact copy
+        else:  # Files does not exist
+            # Create file log
+            create_file(source_file, replica_file)
+            print(f'Created: {source_file}')
+
+
+def delete_invalid_files_from_replica(replica_folder_path, folders_to_delete, files_to_delete):
+    for elem in files_to_delete.items():
+        if not elem[1]:
+            replica_file = join(replica_folder_path, elem[0])
+            remove_file(replica_file)
+            print(f'Deleted: {replica_file}')
+
+    for elem in folders_to_delete.items():
+        if not elem[1]:
+            replica_folder = join(replica_folder_path, elem[0])
+            remove_folder(replica_folder)
+            print(f'Deleted: {replica_folder}')
