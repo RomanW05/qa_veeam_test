2a85069ffae34191d46f89d13c12890fbaeee4b0
Roman <romanwn@mail.com>
2024-01-15 22:54:00 +0200

    Added create/copy file

--- sync_source_replica.py
+++ sync_source_replica.py
@@ -11,3 +11,12 @@ def checksum_file(file_name):
         data = file_to_check.read()
         md5_returned = hashlib.md5(data).hexdigest()
     return md5_returned
+
+def create_file(source_file_path, replica_file_path, buffer_size=1024):
+    with open(source_file_path, 'rb') as source_file:
+        with open(replica_file_path, 'wb') as replica_file:
+            while True:
+                buffer_data = source_file.read(buffer_size)
+                if not buffer_data:
+                    break
+                replica_file.write(buffer_data)
